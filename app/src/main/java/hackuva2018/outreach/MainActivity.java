b506abd4de5d9e874a9ccc0e63b1a8dbf3144ddf
lauren <laurenphan13@gmail.com>
2018-03-25 06:32:50 -0400

    interface

--- app/src/main/java/hackuva2018/outreach/MainActivity.java
+++ app/src/main/java/hackuva2018/outreach/MainActivity.java
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
